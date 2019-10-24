58562f7fdf0aae9e6505d3e0bc35767a7fe17290
Gergely Kiss-Illes <Kiss-IllesG@cardiff.ac.uk>
2019-10-24 16:04:53 +0100

    Javascript JSON parsing problem has been simplified

---
+++ data.js
@@ -0,0 +1,12 @@
+var data = [
+    {
+        "name": "John Smtih",
+        "projectTitle": "Index",
+        "link": "/mscProject/John_Smith/index.html"
+    },
+    {
+        "name": "Harry Potter",
+        "projectTitle": "Firebolt",
+        "link": "/mscProject/Harry_Potter/index.html"
+    }
+]
---
+++ dataLoader.js
@@ -0,0 +1,4 @@
+var data = "[    {        'name': 'John Smtih',        'projectTitle': 'Index',        'link': '/mscProject/John_Smith/index.html'    }]";
+
+// var mscProjects = JSON.parse(data);
+console.log(data);
