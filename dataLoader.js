22cbfe5a9f2facfb3ca31a3d4372aff28dc7d1d4
Gergely Kiss-Illes <Kiss-IllesG@cardiff.ac.uk>
2019-10-25 12:45:02 +0100

    Javascript DOM manipulation is added 2

--- dataLoader.js
+++ dataLoader.js
@@ -2,13 +2,19 @@ console.log(data);
 
 var length = data.length();
 
-for(var j; j < length; j++){
-    var projectTitle = document.createElement("h1");
-    var studentName = document.createElement("p");
-    var link = document.createElement("a");
-}
+var main = document.getElementsByTagName("main")[0];
 
-var projectTitle = document.createElement("h1");
-var studentName = document.createElement("p");
-var link = document.createElement("a");
+for(var j = 0; j < length; j++){
+    var projectTitle = document.createElement("h1");
+    projectTitle.innerHTML = data[j].projectTitle;
+    var studentName = document.createElement("p");
+    studentName.innerHTML = data[j].name;
+    var link = document.createElement("a");
+    link.setAttribute("href", data[j].link);
+    link.innerHTML = "Show me the project";
+
+    main.appendChild(projectTitle);
+    main.appendChild(studentName);
+    main.appendChild(link);
+}
 
