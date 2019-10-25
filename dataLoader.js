6872d04375bfb7a3da97b57d82d95faeb59e9ee7
Gergely Kiss-Illes <Kiss-IllesG@cardiff.ac.uk>
2019-10-25 11:47:01 +0100

    Iterating the data and revert html

--- dataLoader.js
+++ dataLoader.js
@@ -1,1 +1,14 @@
 console.log(data);
+
+var length = data.length();
+
+for(var j; j < length; j++){
+    var projectTitle = document.createElement("h1");
+    var studentName = document.createElement("p");
+    var link = document.createElement("a");
+}
+
+var projectTitle = document.createElement("h1");
+var studentName = document.createElement("p");
+var link = document.createElement("a");
+
