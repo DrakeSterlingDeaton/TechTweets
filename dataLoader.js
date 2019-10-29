e8c3a85d9fb9d9d7dc1cc85e2dbf77a88de6be19
Gergely Kiss-Illes <Kiss-IllesG@cardiff.ac.uk>
2019-10-29 16:39:24 +0000

    dataloader generates html with styling 1.0

--- dataLoader.js
+++ dataLoader.js
@@ -18,3 +18,51 @@ for(var j = 0; j < length; j++){
     main.appendChild(studentName);
     main.appendChild(link);
 }
+
+var projectsOuterBox = document.createElement("div");
+projectsOuterBox.setAttribute("class", "projectsOuterBox");
+var projectsInnerBox = document.createElement("div");
+projectsInnerBox.setAttribute("class", "projectsInnerBox");
+var wholeList = document.createElement("ul");
+wholeList.setAttribute("class", "wholeList");
+
+for(var j = 0; j < length; j++){
+    var studentList = document.createElement("li");
+    studentList.setAttribute("class", "studentList");
+
+    var studentNameBox = document.createElement("div");
+    studentNameBox.setAttribute("class", "studentNameBox");
+
+    var studentName = document.createElement("p");
+    studentName.setAttribute("class", "studentName");
+
+    var studentPDBox = document.createElement("div");
+    studentPDBox.setAttribute("class", "studentPDBox");
+
+    var studentProDesc = document.createElement("p");
+    studentProDesc.setAttribute("class", "studentProDesc");
+
+    var link = document.createElement("a");
+    link.href = data[j].link;
+    link.innerHTML = "Show me the project";
+
+    studentName.innerHTML = data[j].name;
+    studentProDesc.innerHTML = data[j].projectTitle;
+
+    studentNameBox.appendChild(studentName);
+    studentPDBox.appendChild(studentProDesc);
+
+    studentList.appendChild(studentNameBox);
+    studentList.appendChild(studentPDBox);
+    studentList.appendChild(link);
+
+    wholeList.appendChild(studentList);
+}
+
+projectsInnerBox.appendChild(wholeList);
+projectsOuterBox.appendChild(projectsInnerBox);
+main.appendChild(projectsOuterBox);
+
+
+
+
