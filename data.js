92c8c76eb707ca01e4400ea96a885dc82ddcbad4
Ubuntu <ubuntu@ip-172-31-86-141.ec2.internal>
2019-11-03 11:10:38 +0000

    Made changes to contents of data.js JSON

--- data.js
+++ data.js
@@ -1,50 +1,50 @@
 data = [
     {
         name: "John Smith",
-        projectTitle: "Index",
+        projectTitle: "A Website Devoted to One of the Most Common Names in British History",
         link: "mscProjects/John_Smith/index.html",
-        description: "Indexing project blablabla"
+        description: "Viverra mauris in aliquam sem. Sit amet consectetur adipiscing elit ut. Mi proin sed libero enim sed. Arcu bibendum at varius vel pharetra vel turpis nunc. Viverra aliquet eget sit amet tellus cras adipiscing. Ornare massa eget egestas purus. Leo duis ut diam quam nulla porttitor massa id neque. Purus gravida quis blandit turpis cursus in hac. Eleifend donec pretium vulputate sapien nec. In tellus integer feugiat scelerisque varius morbi. Massa massa ultricies mi quis hendrerit dolor magna eget. Tortor consequat id porta nibh venenatis cras sed felis. Nibh cras pulvinar mattis nunc sed blandit libero volutpat. Tellus molestie nunc non blandit massa enim nec dui nunc."
+    },
+    {
+        name: "David Wilson",
+        projectTitle: "A Website on Explosions of All Shapes and Sizes",
+        link: "mscProjects/David_Wilson/index.html",
+        description: "Mi bibendum neque egestas congue quisque egestas diam in arcu. Tincidunt dui ut ornare lectus sit amet. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. A scelerisque purus semper eget duis at. Elit ullamcorper dignissim cras tincidunt. Gravida arcu ac tortor dignissim convallis. Amet risus nullam eget felis eget nunc lobortis. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus. Lacus viverra vitae congue eu consequat. Nibh tellus molestie nunc non blandit massa enim nec. Pharetra pharetra massa massa ultricies. Enim nec dui nunc mattis enim ut tellus. Nisl suscipit adipiscing bibendum est ultricies integer quis. Nisl nisi scelerisque eu ultrices vitae auctor eu."
+    },
+    {
+        name: "Joseph Abraham",
+        projectTitle: "Biblical Disgressions and Comparisons of Faith",
+        link: "mscProjects/Joseph_Abraham/index.html",
+        description: "Odio ut sem nulla pharetra diam sit amet. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Luctus accumsan tortor posuere ac ut. Nisl pretium fusce id velit ut tortor pretium viverra suspendisse. Sem fringilla ut morbi tincidunt augue interdum velit euismod. Praesent semper feugiat nibh sed pulvinar proin gravida. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Maecenas ultricies mi eget mauris pharetra. Etiam sit amet nisl purus. Ut venenatis tellus in metus. In hac habitasse platea dictumst vestibulum rhoncus est."
+    },
+    {
+        name: "Alex Thomas",
+        projectTitle: "An Introduction to Modern JavaScript - ECMAScript10",
+        link: "mscProjects/Alex_Thomas/index.html",
+        description: "Scelerisque viverra mauris in aliquam sem fringilla. Pellentesque dignissim enim sit amet. Aliquam nulla facilisi cras fermentum. Nulla facilisi cras fermentum odio eu. Mi sit amet mauris commodo. Vulputate sapien nec sagittis aliquam malesuada bibendum. Quam viverra orci sagittis eu volutpat odio facilisis. Eget egestas purus viverra accumsan in nisl nisi. Ac turpis egestas integer eget aliquet. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Amet porttitor eget dolor morbi non arcu risus quis varius. Eget magna fermentum iaculis eu non. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sit amet aliquam id diam. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Imperdiet nulla malesuada pellentesque elit eget gravida. Scelerisque viverra mauris in aliquam sem fringilla ut. Fames ac turpis egestas sed tempus urna. Scelerisque fermentum dui faucibus in ornare."
     },
     {
         name: "Harry Potter",
-        projectTitle: "Firebolt",
+        projectTitle: "An Encyclopedia on Fantastic Beasts and Where to Find Them",
         link: "mscProjects/Harry_Potter/index.html",
-        description: "Watch em magic wands shoot balls of FIREE!!"
+        description: "Posuere lorem ipsum dolor sit amet consectetur. Malesuada fames ac turpis egestas. Egestas quis ipsum suspendisse ultrices. Ultrices tincidunt arcu non sodales neque sodales. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vel pretium lectus quam id leo in. Leo vel fringilla est ullamcorper eget nulla facilisi. Iaculis at erat pellentesque adipiscing. Magna eget est lorem ipsum dolor sit amet consectetur. Placerat duis ultricies lacus sed turpis tincidunt id. Sit amet commodo nulla facilisi nullam vehicula ipsum. Gravida in fermentum et sollicitudin ac orci phasellus."
     },
     {
-        name: "Joker McBatman",
-        projectTitle: "Explosions",
-        link: "mscProjects/Joker_McBatman/index.html",
-        description: "Gotham City never had it this bad."
+        name: "George Darwin",
+        projectTitle: "A Compendium on the Darwin Awards",
+        link: "mscProjects/George_Darwin/index.html",
+        description: "Fames ac turpis egestas sed tempus urna et pharetra pharetra. Nulla pharetra diam sit amet nisl suscipit. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat. Odio ut sem nulla pharetra diam sit amet nisl suscipit. Nisi est sit amet facilisis magna. Pellentesque adipiscing commodo elit at imperdiet dui. Tellus rutrum tellus pellentesque eu tincidunt tortor. Id velit ut tortor pretium viverra suspendisse potenti. Tristique nulla aliquet enim tortor at auctor urna nunc id. Nec ultrices dui sapien eget mi proin sed libero enim. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Enim nulla aliquet porttitor lacus. Sed velit dignissim sodales ut eu sem. Proin libero nunc consequat interdum varius sit."
     },
     {
-        name: "God Himself",
-        projectTitle: "The Great Creation",
-        link: "mscProjects/God_Himself/index.html",
-        description: "DIY guide to build your own magical world!"
-    },
-    {
-        name: "Vladimir Putin",
-        projectTitle: "In Soviet Russia...",
-        link: "mscProjects/Vladimir_Putin/index.html",
-        description: "Ruling the Great Nation. Then ruling it again. And again. And again."
-    },
-    {
-        name: "Charles Darwin",
-        projectTitle: "Darwin Awards",
-        link: "mscProjects/Charles_Darwin/index.html",
-        description: "There's always a bigger idiot."
-    },
-    {
-        name: "Cookie Monster",
-        projectTitle: "Motha friggin' COOKIES",
-        link: "mscProjects/Cookie_Monster/index.html",
-        description: "Best recipes for the worlds most glorious and tasty cookies."
+        name: "Bob Jones",
+        projectTitle: "A Guide to the Paintings of the Romantic Period",
+        link: "mscProjects/Bob_Jones/index.html",
+        description: "Adipiscing elit ut aliquam purus sit amet luctus. At erat pellentesque adipiscing commodo. A condimentum vitae sapien pellentesque habitant. Facilisi etiam dignissim diam quis enim. Mauris vitae ultricies leo integer. Pulvinar etiam non quam lacus. Feugiat in ante metus dictum at tempor commodo. Orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam. Enim nulla aliquet porttitor lacus."
     },
     {
         name: "Zefram Cochrane",
         projectTitle: "Basic Warp Field Theory and the Advancements in FTL Travel",
         link: "mscProjects/Zefram_Cochrane/index.html",
-        description: "Read this and you too might have them pointy-eared logic munchers knocking on your doorstep."
+        description: "Augue mauris augue neque gravida in. Cursus mattis molestie a iaculis at erat pellentesque adipiscing. Enim eu turpis egestas pretium aenean pharetra magna ac placerat. Et netus et malesuada fames. Nunc mattis enim ut tellus. Senectus et netus et malesuada. Magna sit amet purus gravida quis blandit turpis cursus. Gravida neque convallis a cras semper auctor neque vitae. Et tortor consequat id porta. Erat nam at lectus urna duis convallis convallis tellus id. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Elit sed vulputate mi sit amet mauris commodo quis imperdiet."
     }
 ];
