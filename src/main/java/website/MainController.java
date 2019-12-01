4b0288cb022d796f3d67254eb4c7b903bbf6e2bb
DrakeDeaton <deatond@cardiff.ac.uk>
2019-12-01 15:34:15 +0000

    test

---
+++ src/main/java/website/MainController.java
@@ -0,0 +1,19 @@
+package website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@SpringBootApplication
+public class MainController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainController.class, args);
+    }
+
+    @GetMapping("/")
+    public ModelAndView greetingResponse(){
+        return new ModelAndView("index.html");
+    }
+}
